490f40ff1c1454ff70b135de81ab738c689062c4
Joanna Oleszczuk <jooleszczuk@wp.pl>
2023-01-28 21:15:03 +0100

    Move describe method to the class

--- main.py
+++ main.py
@@ -1,38 +1,40 @@
+
+
 class Mountain:
     name = ""
     high = 0
     country = ""
-    schelter = None
+    has_schelter = False
 
     def get_high_to_the_top(self):
         currentHigh = int(input("Enter your current high: "))
-        if currentHigh > self.high:
+        if currentHigh > self.high: #write an exception!
             print("You enter wrong high")
         else:
             return self.high-currentHigh
+    def describe(self):
+        print(self.name, self.high, self.country, self.has_schelter)
 
 mountain = Mountain()
-mountain.name = "Sniezka"
 mountain.high = 1602
+mountain.name = "Sniezka"
 mountain.country = "Poland"
-mountain.schelter = True
-
-def describe_mountain1(mountain):
-    print(mountain.name, mountain.high, mountain.country, mountain.schelter)
-describe_mountain1(mountain)
+mountain.has_schelter = True
 
 print(mountain.get_high_to_the_top())
+mountain.describe()
+
 
 mountain2 = Mountain()
 mountain2.name = "Matterhorn"
 mountain2.high = 4478
 mountain2.country = "Switzerland"
-mountain2.schelter = False
+mountain2.has_schelter = False
 print(mountain2.get_high_to_the_top())
 
 mountain3 = Mountain()
 mountain3.name = "Gerlach"
 mountain3.high = 2655
 mountain3.country = "Slovakia"
-mountain3.schelter = False
+mountain3.has_schelter = False
 print(mountain3.get_high_to_the_top())
